e8f90c90eee6ea8402d25902463dc40a19a8d375
Jiaqi Liang <liang9804angel@126.com>
2026-05-05 11:15:46 +0200

    homework 0505

--- notebooks/volume_validation.py
+++ notebooks/volume_validation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9cceb005ac49db3dd5677d5f54389a01d622fd3f34117ed87647d54acd7f8e7
-size 7339
+oid sha256:31af6fdcc4ac150b74247510d54fd23ba9cf7b8796f7a7d0ba9efc104894ef8e
+size 7661
---
+++ notebooks/volume_validation_allpixels.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d36952675d26aa19adaae7a0e198563fbb2009a7167884b82ecf905de83be2d
+size 7076
